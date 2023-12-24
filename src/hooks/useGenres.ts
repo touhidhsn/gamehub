d8652da5646a59b610a2569be1334c0f5fc856d9
Touhid Hasan <touhidhsn@gmail.com>
2023-12-24 23:48:27 +0600

    hooks has been updated with query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
-import useData from "./useData";
+import { useQuery } from 'react-query';
+import apiClient from '../services/api-client';
+import { FetchResponse } from './useData';
 
 export interface Genre {
   id: string;
@@ -6,6 +8,12 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () =>
+  useQuery({
+    queryKey: ['genres'],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>('/genres').then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24 hours
+  });
 
 export default useGenres;
