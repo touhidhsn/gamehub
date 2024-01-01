1502a78da17664e4a88da8edc775a06555f91d90
Touhid Hasan <touhid1@outlook.comgit config --global user.email>
2024-01-01 22:59:52 +0600

    setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import App from './App.tsx';
+
 import './index.css';
 import theme from './theme.ts';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes.tsx';
 
 const queryClinet = new QueryClient();
 
@@ -13,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClinet}>
-        <App />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
